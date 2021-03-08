dde1c1de64e0f84fd8d969993335fe2f50cc2997
nicholasgacicia <nicholasgacicia@gmail.com>
2021-03-08 17:24:01 -0500

    Add upcoming shows to DB

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
 const PORT = process.env.PORT || 4000;
 const routes = require('./routes');
 
-// Home Route
+
+// ~Middleware
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+
+
+// ~Home Route
 app.get('/', (req, res) => {
   res.send('<h1>YEMStats API</h1>');
 });
 
-// Shows API Routes
+// ~Shows API Routes
 app.use('/api/shows', routes.shows);
 
 
-// App Listener
+// ~App Listener
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
